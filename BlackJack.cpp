f74e91fe796265d3f3ae6ca6df3b505f110171c7
aadityasrinivas <83307039+aadityasrinivas@users.noreply.github.com>
2022-06-19 21:41:33 +0530

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -9,17 +9,19 @@ struct scores
     int size;
     int sum;
     bool done;
+    int csuit[5];
 };
 typedef struct scores sc;
 
 sc player, dealer;
-
+string card[] = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+string suit[] = {"Clubs", "Spades", "Hearts", "Diamond"};
 void addCardPlayer()
 {
-    srand(time(0));
-    if(player.size<=5)
+    if(player.size<5)
     {
-        player.hand[player.size] = 1 + (rand()%11);
+        player.hand[player.size] = 1 + (rand()%13);
+        player.csuit[player.size] = 1 + (rand()%4);
         player.size++;
     }
     else
@@ -30,9 +32,10 @@ void addCardPlayer()
 }
 void addCardDealer()
 {
-    if(dealer.size<=5)
+    if(dealer.size<5)
     {
-        dealer.hand[dealer.size] = 1 + (rand()%11);
+        dealer.hand[dealer.size] = 1 + (rand()%13);
+        dealer.csuit[dealer.size] = 1 + (rand()%4);
         dealer.size++;
     }
     else
@@ -44,13 +47,54 @@ void addCardDealer()
 
 void sumCards()
 {
-    int i, j;
+    int i, j, count=0;
     player.sum = 0;
     dealer.sum =0;
     for(i=0; i<player.size; i++)
+    {
+        if(player.hand[i]>10)
+        {
+            player.sum+=10;
+            continue;
+        }
+        if(player.hand[i]==1)
+        {
+            count++;
+            continue;
+        }
         player.sum+= player.hand[i];
+    }
+    while(count>0)
+    {
+        if((player.sum+11)>21)
+            player.sum+=1;
+        else
+            player.sum+=11;
+        count--;
+    }
+    count =0;
     for(j=0; j<dealer.size; j++)
+    {
+        if(dealer.hand[j]>10)
+        {
+            dealer.sum+=10;
+            continue;
+        }
+        if(dealer.hand[j]==1)
+        {
+            count++;
+            continue;
+        }
         dealer.sum+= dealer.hand[j];
+    }
+    while(count>0)
+    {
+        if((dealer.sum+11)>21)
+            dealer.sum+=1;
+        else
+            dealer.sum+=11;
+        count--;
+    }
     cout<<"Player Score is: "<<player.sum<<endl;
     cout<<"Dealer Score is: "<<dealer.sum<<endl;
 }
@@ -60,12 +104,12 @@ void printCards()
     int i;
     cout<<"Player Cards are\n";
     for(i=0; i<player.size; i++)
-        cout<<player.hand[i]<<" - ";
-    cout<<endl;
+        cout<<card[player.hand[i]-1]<<" of "<<suit[player.csuit[i]-1]<<" - ";
+    cout<<"\b \b"<<"\b \b"<<endl;
     cout<<"Dealer Cards are\n";
     for(i=0; i<dealer.size; i++)
-        cout<<dealer.hand[i]<<" - ";
-    cout<<endl;
+        cout<<card[dealer.hand[i]-1]<<" of "<<suit[dealer.csuit[i]-1]<<" - ";
+    cout<<"\b \b"<<"\b \b"<<endl;
 }
 
 int checkBust()
@@ -99,41 +143,43 @@ int checkBlackJack()
     else return 0;
 }
 
-void checkWin()
+int checkWin()
 {
     if(player.sum == 21 && dealer.sum == 21)
     {
         cout<<"Tie\n";
-        return;
+        return 1;
     }
     if(checkBlackJack())
-        return;
-    if((player.done == true && dealer.done ==true ) || (player.size==5 && dealer.size == 5))
+        return 1;
+    if((player.done == true && dealer.done ==true ) || (player.size==4 && dealer.size == 4))
     {
         if(player.sum>dealer.sum)
         {
             cout<<"Player Wins\n";
-            return;
+            return 1;
         }
         else if(player.sum<dealer.sum)
         {
             cout<<"Dealer Wins\n";
-            return;
+            return 1;
         }
         else
         {
             cout<<"Tie\n";
-            return;
+            return 1;
         }
     }
+    return 0;
 }
 
 void playGame()
 {
+    srand(time(0));
     char hit, stand;
     addCardPlayer();
-    addCardPlayer();
     addCardDealer();
+    addCardPlayer();
     addCardDealer();
     printCards();
     sumCards();
@@ -173,19 +219,44 @@ void playGame()
                 sumCards();
                 if(checkBust())
                     return;
-                if(dealer.sum>=17)
-                    dealer.done = true;
-                checkWin();
+                //if(dealer.sum>=17)
+                    //dealer.done = true;
+                if(checkWin())
+                    return;
                     //return;
             }
-        }
+            else if(player.done == true && dealer.sum<player.sum)
+            {
+                addCardDealer();
+                printCards();
+                sumCards();
+                if(checkBust())
+                    return;
+                if(checkWin())
+                    return;
+            }
+            else
+            {
+                dealer.done  = true;
+                if(checkWin())
+                    return;
+            }
+    }
 }
 int main()
 {
+    char ch;
     playGame();
+    // while(1)
+    // {
+    //     cout<<"Would You Like To Play Another Game?\nYes(y)/No(n)";
+    //     cin>>ch;
+    //     if(ch == 'y')
+    //     {
+    //         system("clear");
+    //         playGame();
+    //     }
+    //     else 
+    //         return 0;
+    // }
 }
-
-
-
-
-
